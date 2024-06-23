1d8cb6c728a54a1765182b03e5e5f2f51652d5d2
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-23 23:09:12 +0530

    Displaying error messages on the UI

--- Formik_Library/src/components/YoutubeForm.js
+++ Formik_Library/src/components/YoutubeForm.js
@@ -1,23 +1,27 @@
 /*
-  * Using ES6 shorthand syntax for object literals
-  * Refactoring the code:
-  * moving the property of the useFormik hook at the top of the code
+  ! Displaying the Error on the UI
+  * Formik provides an errors object that, similar to the values object, contains key-value pairs for each 
+  * form field, where the keys are the form field names and the values are the error messages specified 
+  * in the validate function.
+  * to display the error on the UI we need to apply the object.notations [FormikObject.error.(fieldname)]
 */
-
+/*
+  * The formicObject provided by the formik hook contains lot of properties and method (like values, errors, 
+  * handleSubmit, handleChange etc). it is this formicObject which provides the errors object which is used
+  * to display error message (created in previous lecture) on the UI.
+  * this error message will be populated with the same values as specified in the validate function 
+*/
 import React from 'react'
-import { useFormik } from 'formik'
+import { Formik, useFormik } from 'formik'
 
-// Placing the initialValue object at the top
 const initialValues =  {
   name: '',
   email: '',
   channel: '',
 };
-// Placing the onSubmit object at the top
 const onSubmit = (values) => {
   console.log('the form data provided by the user is ',values);
 };
-// Placing the validate object at the top
 const validate =  (values) => {
   const errors = {};
 
@@ -37,27 +41,55 @@ const validate =  (values) => {
   return errors;
 }
 
-// P
 function YoutubeForm() {
   const FormikObject = useFormik({
-    // we are using ES6 shortHand Syntax to define the object literals
     initialValues,
     onSubmit,
     validate,
   }); 
 
-  
+  // Logging the  the values on the console:
+  console.log('the data entered by the user', FormikObject.values);
+  // Logging the error on the console:
+  console.log('the error message', FormikObject.errors);
+
   return (
     <div>
         <form onSubmit={FormikObject.handleSubmit}>
-            <label htmlFor='name'>Name:</label>
-            <input type='text' id ='name' name='name' onChange = {FormikObject.handleChange} value = {FormikObject.values.name}/>
+          <div className='form-control'>
+              <label htmlFor='name'>Name:</label>
+              <input type='text' id ='name' name='name' onChange = {FormikObject.handleChange} value = {FormikObject.values.name}/>
+              {/* Displaying the error: if user clicked on the submit button without providing the values to the field
+                  then the error object has the value 'Required' for each field -> as done in previous commit .*/}
+              {/* By default the value to the FormikObject.errors.(fieldName) is Required. */}
+              {/*  Error messages are displayed conditionally in JSX under each input element. If an error message 
+                  exists for a field (e.g., formik.errors.name), it is rendered; otherwise, it renders null. */}
+              {/* To properly display error messages, each form control is enclosed in a div tag with the 
+                  class form-control, and error messages are given the class error.  */}
+              {/* If user has provided the value to the input tag then FormikObject.errors.name will be equal to 
+                  null. then the 2nd property will be rendered */}
+              {
+                FormikObject.errors.name ? (<div className='error'>{FormikObject.errors.name}</div>):(null)
+              }
+          </div>
 
-            <label htmlFor='email'>Email:</label>
-            <input type='email' id ='email' name='email' onChange = {FormikObject.handleChange} value = {FormikObject.values.email}/>
 
-            <label htmlFor='channel'>Channel:</label>
-            <input type='text' id ='channel' name='channel' onChange = {FormikObject.handleChange} value = {FormikObject.values.channel} />
+          <div className='form-control'>
+              <label htmlFor='email'>Email:</label>
+              <input type='email' id ='email' name='email' onChange = {FormikObject.handleChange} value = {FormikObject.values.email}/>
+              {
+                FormikObject.errors.email ? (<div className='error'>{FormikObject.errors.email}</div>):(null)
+              }
+
+          </div>
+            
+          <div className='form-control'>
+              <label htmlFor='channel'>Channel:</label>
+              <input type='text' id ='channel' name='channel' onChange = {FormikObject.handleChange} value = {FormikObject.values.channel} />
+              {
+                FormikObject.errors.channel ? (<div className='error'> {FormikObject.errors.channel}</div>):(null)
+              }
+          </div>
 
             <button type = 'submit'>Submit</button>
         </form>
