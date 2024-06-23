9b9473562e6de9a13f81e29784cdbc4b5858b785
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-23 18:25:05 +0530

    Handling the submission of form using onSubmit handler used in form tag and onSubmit property used in the useFormik Hook

--- Formik_Library/src/components/YoutubeForm.js
+++ Formik_Library/src/components/YoutubeForm.js
@@ -1,56 +1,48 @@
 /*
-  * Lecture 4 : Managing the state 
-  * * In the youtube form we have 3 field => name, email, channel. To track these 3 
-    * fields we need the state variable for that. But in the case of formik => no need 
-    * to create the state variable rather it is useFormik() which return the object (FormikObject), 
-    * which will track the state.
+  * Lecture 4 : Handling the submission of form 
+    *      There are 2 step to handle the submission using formik:
+    *       1. specify the onSubmit handler on the form tag whose value is equal to 
+    *               FormikObject.handleSubmit (FormikObject is the object returned by hook) => Line 45
+    * 
+    *       2. Define the onSubmit property in the useFormik hook. the property 
+    *           calls the callBack function. this callBack function receive 'values'
+    *           property as the parameter. values stores the current state value of 
+    *           each field.     =================> Line 37
+    *           the values incorporate the all values of field which we have accessed
+    *           using formik.values
+    *       3. when we click on the submit button => formik will automatically execute
+    *           the onSubmit property defined inside the useHook
 */
 
 /*
-    * Step 1: as discussed the useFormik hook consume the object. so the first property
-    * we need to pass is the initialValues (Camel_Case). 
-    ! Line 30
-    * the initialValue property is an object which takes the initial values for all the form field => 
-    * name, email, channel. 
-    *     Note: the properties for initialValue corresponds to the name attribute to the form fields.
-    */
-
-    /*
-      ! For managing the state
-        * we need to add the onChange and values prop for each of the field property.
-        ! onChange={formik.handleChange}. handleChange is the method give by hook.
-        ! values = {formik.values.name}. values is property provided by the hook
+   
 */
 import React from 'react'
 import { useFormik } from 'formik'
 
 function YoutubeForm() {
-
-  /* 
-    * passing the property to the hook => initialValue (CamelCase). the initialValue property takes the object.
-    * the object contains the key which are identical to the name attribute of all the input fields
-  */ 
   const FormikObject = useFormik({
     initialValues: {
       name: '',
       email: '',
       channel: '',
     },
-  });
+    /* 
+      * Defining the onSubmit property: this property is a method which automatically receives the 
+      * Form state as its argument -> in the name of `values`.
+      * as told above the onSubmit is an method/Function. using the arrow function with values as parameter passed.
+      * the values is an inbuilt object provided by the FormikObject.
+      * the onSubmit button is automatically executed when the we click the submit button
+      */
+    onSubmit: (values) => {
+      console.log('the form data provided by the user is ',values);
+    } 
+    });
 
-  console.log('The values entered to the field is stroed in the values object', FormikObject.values)
   
   return (
     <div>
-        <form>
-
-            {/* Adding the onChange and values prop for each of the field */}
-            {/* This is the position where formik Constant will come to picture. for the onChange we assign ?
-                value as formikObject.handleChange and for the value attribute => it shall be formikObject.values.(name of attribute).
-                By assigning the value => the formik will automatically trace the value for You on change of input field [which was done using useState]
-                the onchange handle will track the value of each field and interally updates the value to the values parameter. */}
-
-                {/* the values parameter is an object which store the value of each field. To see it run the code and check the console screen. Line 41 */}
+        <form onSubmit={FormikObject.handleSubmit}>
             <label htmlFor='name'>Name:</label>
             <input type='text' id ='name' name='name' onChange = {FormikObject.handleChange} value = {FormikObject.values.name}/>
 
