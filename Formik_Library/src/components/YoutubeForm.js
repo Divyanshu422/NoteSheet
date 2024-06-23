6e0fa40f2502eed7eee107421a7418fcc05b91ce
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-23 17:44:17 +0530

    Managing the state of fields using FormikObject -> which provide handleChange, values and the hook consume the initialValues Props

--- Formik_Library/src/components/YoutubeForm.js
+++ Formik_Library/src/components/YoutubeForm.js
@@ -1,41 +1,64 @@
 /*
-  * Lecture 3: 
-  * In this video we have installed the formik library using  `npm install formik --save'
-
-  * Step 1: Import the useFormik hook form the formik.  (Line 26)
-  *                    -  A hook is a function hence it must be called
-  *                    -  the useFormik hook consume the object and return the object.
-  *       the useFormik hook takes an object as a parameter and return
-  *       a object which contains varities of methods and properties which are used in the 
-  *       Form (like values property, onSubmit etc).
-  *       In our code we have called the return object as FormikObject ((Line 26))
+  * Lecture 4 : Managing the state 
+  * * In the youtube form we have 3 field => name, email, channel. To track these 3 
+    * fields we need the state variable for that. But in the case of formik => no need 
+    * to create the state variable rather it is useFormik() which return the object (FormikObject), 
+    * which will track the state.
 */
 
 /*
-        * the useFormik hook takes the object as an input and return a object. in my
-        * code I have named the returned object as formikObject (Line 26). this object will 
-        * Helps in managing the state, handling the submission and validation + error
-        * message. 
+    * Step 1: as discussed the useFormik hook consume the object. so the first property
+    * we need to pass is the initialValues (Camel_Case). 
+    ! Line 30
+    * the initialValue property is an object which takes the initial values for all the form field => 
+    * name, email, channel. 
+    *     Note: the properties for initialValue corresponds to the name attribute to the form fields.
+    */
+
+    /*
+      ! For managing the state
+        * we need to add the onChange and values prop for each of the field property.
+        ! onChange={formik.handleChange}. handleChange is the method give by hook.
+        ! values = {formik.values.name}. values is property provided by the hook
 */
 import React from 'react'
 import { useFormik } from 'formik'
 
 function YoutubeForm() {
 
-  // Calling the hook. the useFormik hook consume the object
-  const FormikObject = useFormik({})
+  /* 
+    * passing the property to the hook => initialValue (CamelCase). the initialValue property takes the object.
+    * the object contains the key which are identical to the name attribute of all the input fields
+  */ 
+  const FormikObject = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      channel: '',
+    },
+  });
+
+  console.log('The values entered to the field is stroed in the values object', FormikObject.values)
   
   return (
     <div>
         <form>
-            <label htmlfor='name'>Name:</label>
-            <input type='text' id ='name' name='name'/>
 
-            <label htmlfhtmlor='email'>Email:</label>
-            <input type='email' id ='email' name='email'/>
+            {/* Adding the onChange and values prop for each of the field */}
+            {/* This is the position where formik Constant will come to picture. for the onChange we assign ?
+                value as formikObject.handleChange and for the value attribute => it shall be formikObject.values.(name of attribute).
+                By assigning the value => the formik will automatically trace the value for You on change of input field [which was done using useState]
+                the onchange handle will track the value of each field and interally updates the value to the values parameter. */}
 
-            <label htmlfor='channel'>Channel:</label>
-            <input type='text' id ='channel' name='channel' />
+                {/* the values parameter is an object which store the value of each field. To see it run the code and check the console screen. Line 41 */}
+            <label htmlFor='name'>Name:</label>
+            <input type='text' id ='name' name='name' onChange = {FormikObject.handleChange} value = {FormikObject.values.name}/>
+
+            <label htmlFor='email'>Email:</label>
+            <input type='email' id ='email' name='email' onChange = {FormikObject.handleChange} value = {FormikObject.values.email}/>
+
+            <label htmlFor='channel'>Channel:</label>
+            <input type='text' id ='channel' name='channel' onChange = {FormikObject.handleChange} value = {FormikObject.values.channel} />
 
             <button type = 'submit'>Submit</button>
         </form>
