6c123af55ffa34360cdcbb66912bb31a5a477433
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-23 22:48:29 +0530

    Using ES6 ShortHand Syntax to define object Literals

--- Formik_Library/src/components/YoutubeForm.js
+++ Formik_Library/src/components/YoutubeForm.js
@@ -1,58 +1,50 @@
 /*
-  * Lecture 5 and 6: Form validation
-  *   Formik allows the developer to define the validation function which is passed to the hook using the validate property.
-  *   the validate property just like the onSubmit property, receives the `values` object as its parameter. This `values object`
-  *   contains the current values of the form fields.
-*/
-
-/*
- ! What validate function comprises of which is passed to validate property of the hook?
- * 1. The validate function must return an object called errors. This object will hold error messages for each form 
- *    field that fails validation.
- * 2. The keys of the errors object should correspond to the names of the form fields. For example, if the form fields are 
- *    name, email, and channel, the errors object should have keys like errors.name, errors.email, and errors.channel.
- * 3. The values of the keys in the errors object should be strings that represent the error messages. For example, if the 
- *    name field is empty, errors.name might be set to "required".
+  * Using ES6 shorthand syntax for object literals
+  * Refactoring the code:
+  * moving the property of the useFormik hook at the top of the code
 */
 
 import React from 'react'
 import { useFormik } from 'formik'
 
+// Placing the initialValue object at the top
+const initialValues =  {
+  name: '',
+  email: '',
+  channel: '',
+};
+// Placing the onSubmit object at the top
+const onSubmit = (values) => {
+  console.log('the form data provided by the user is ',values);
+};
+// Placing the validate object at the top
+const validate =  (values) => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = 'Required';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = 'Invalid email format';
+  }
+
+  if (!values.channel) {
+    errors.channel = 'Required';
+  }
+  return errors;
+}
+
+// P
 function YoutubeForm() {
   const FormikObject = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      channel: '',
-    },
-    onSubmit: (values) => {
-      console.log('the form data provided by the user is ',values);
-    },
-    validate: (values) => {
-      // Creating the object with the name errors -> which is returned
-      const errors = {};
-      
-      // checking for name field => if values.name is not present then the errors.name hold the error Required.
-      if (!values.name) {
-        // the keys of the error object need to same as the values object
-        errors.name = 'Required';
-      }
-    
-      if (!values.email) {
-        errors.email = 'Required';
-      } 
-      // Checking the email for the regex
-      else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-        errors.email = 'Invalid email format';
-      }
-    
-      if (!values.channel) {
-        errors.channel = 'Required';
-      }
-      // retuning the errors object.
-      return errors;
-    }
-  });
+    // we are using ES6 shortHand Syntax to define the object literals
+    initialValues,
+    onSubmit,
+    validate,
+  }); 
 
   
   return (
