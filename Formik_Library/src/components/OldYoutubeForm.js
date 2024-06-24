a6d31dd113c10fef6225efad55dfa25b76e0b022
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-24 16:28:05 +0530

    Reducing the boiler plate code -> onChange, values and onBlur with the getFieldsProps

--- Formik_Library/src/components/OldYoutubeForm.js
+++ Formik_Library/src/components/OldYoutubeForm.js
@@ -1,29 +1,4 @@
-/*
-  * Simplifying the code using the boilerPlate code
-  * 1. we have removed the validate Function from the code as we are using the validate Schema.
-  * 2. In the code there are 3 props in the input fields which are almost similar which are:
-  *               - onChange
-  *               - onBlur
-  *               - value prop
-  ToDo:  
-    <input type='email' id ='email' name='email' onChange = {FormikObject.handleChange} value = {FormikObject.values.email}/>
-*/
 
-/*
-  * So formik is going to provide `getFieldprops` which will behind the schene add these 3 props
-  * to the input field.  Instead of manually adding onChange, onBlur, and value props to each 
-  * form field, getFieldProps consolidates these operations into a single line of code per field.
-  * Requires passing the name attribute of each form field as an argument to getFieldProps, 
-  * which then handles the assignment of necessary props internally.
-*/
-
-/*
-  ! Earlier code:
-    *  <input type='text' id ='name' name='name' onChange = {FormikObject.handleChange} value = {FormikObject.values.name} onBlur= {FormikObject.handleBlur}/>
-  ! New code:
-    *  <input type='text' id ='name' name='name' { ...FormikObject.getFieldProps(name)}/>
-    *       use of spread operator is mandatory
-*/
 
 import React from 'react'
 import { Formik, useFormik } from 'formik'
@@ -47,9 +22,26 @@ const initialValues =  {
 const onSubmit = (values) => {
   console.log('the form data provided by the user is ',values);
 };
+const validate =  (values) => {
+  const errors = {};
 
+  if (!values.name) {
+    errors.name = 'Required';
+  }
 
-function YoutubeForm() {
+  if (!values.email) {
+    errors.email = 'Required';
+  }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = 'Invalid email format';
+  }
+
+  if (!values.channel) {
+    errors.channel = 'Required';
+  }
+  return errors;
+}
+
+function OldYoutubeForm() {
   const FormikObject = useFormik({
     initialValues,
     onSubmit,
@@ -61,7 +53,7 @@ function YoutubeForm() {
         <form onSubmit={FormikObject.handleSubmit}>
           <div className='form-control'>
               <label htmlFor='name'>Name:</label>
-              <input type='text' id ='name' name='name' { ...FormikObject.getFieldProps('name')}/>
+              <input type='text' id ='name' name='name' onChange = {FormikObject.handleChange} value = {FormikObject.values.name} onBlur= {FormikObject.handleBlur}/>
            
               {
                 FormikObject.touched.name && FormikObject.errors.name ? (<div className='error'>{FormikObject.errors.name}</div>):(null)
@@ -70,8 +62,7 @@ function YoutubeForm() {
 
           <div className='form-control'>
               <label htmlFor='email'>Email:</label>
-              <input type='email' id ='email' name='email' { ...FormikObject.getFieldProps('email')}/>
-           
+              <input type='email' id ='email' name='email' onChange = {FormikObject.handleChange} value = {FormikObject.values.email}/>
 
               {
                FormikObject.touched.email && FormikObject.errors.email ? (<div className='error'>{FormikObject.errors.email}</div>):(null)
@@ -80,7 +71,7 @@ function YoutubeForm() {
             
           <div className='form-control'>
               <label htmlFor='channel'>Channel:</label>
-              <input type='text' id ='channel' name='channel' { ...FormikObject.getFieldProps('channel')}/>
+              <input type='text' id ='channel' name='channel' onChange = {FormikObject.handleChange} value = {FormikObject.values.channel} />
               {
                 FormikObject.touched.channel && FormikObject.errors.channel ? (<div className='error'> {FormikObject.errors.channel}</div>):(null)
               }
@@ -92,4 +83,4 @@ function YoutubeForm() {
   )
 }
 
-export default YoutubeForm
+export default OldYoutubeForm
