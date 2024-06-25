2454c9c49b4e6a1d924a30b8e3ab973c34d6f0a1
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-25 06:06:48 +0530

    Correct ErrorMessage Component code

--- Formik_Library/src/components/YoutubeForm.js
+++ Formik_Library/src/components/YoutubeForm.js
@@ -55,18 +55,18 @@ function YoutubeForm() {
         <Form>
           <div className='form-control'>
               <label htmlFor='name'>Name:</label>
-              <Form type='text' id ='name' name='name'/>
+              <Field type='text' id ='name' name='name'/>
               <ErrorMessage name = 'name'/>
           </div>
           <div className='form-control'>
               <label htmlFor='email'>Email:</label>
-              <Form type='email' id ='email' name='email' />
+              <Field type='email' id ='email' name='email' />
               <ErrorMessage name = 'email'/>
           </div>
             
           <div className='form-control'>
               <label htmlFor='channel'>Channel:</label>
-              <Form type='text' id ='channel' name='channel' />
+              <Field type='text' id ='channel' name='channel' />
               <ErrorMessage name = 'channel'/>
           </div>
             <button type = 'submit'>Submit</button>
